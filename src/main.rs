a7e4546708a2e1a2004276ba1d616bccde5d73c5
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2023-09-08 12:26:26 +0000

    work with string and array

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
 fn main() {
-    let x:i32 = 12;
-    println!("{}",x);
+    let _x:i32 = 12;
+    let c:char = 'ف';
+    println!("{}",c as i32);
+
+    let name:&str = "Ebrahim";
+    println!("{}",name);
+
+    let family:String = "Barati".to_string();
+    let full_name = name.to_string() + " " +  &family;
+
+    let full_name_2 = format!("{} {}",name,family);
+
+    println!("{}",full_name);
+    println!("{}",full_name_2);
+
+    let arr:[i32;3] = [2,95,63];
+    println!("{:?}",arr);
+
+    let arr_2 = [5;5];
+    println!("{:?}",arr_2);
 }
